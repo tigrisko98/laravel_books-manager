6b24b64241e1a7b9c085bcd29197aa12e9a75c03
tigrisko98 <kostia.tigrisko@gmail.com>
2021-07-14 09:12:34 +0300

    added migrations for books and comments tables

---
+++ database/migrations/2021_07_14_060244_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('author_name');
+            $table->string('slug');
+            $table->string('title');
+            $table->boolean('is_archived');
+            $table->year('publication_year');
+            $table->string('image_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
